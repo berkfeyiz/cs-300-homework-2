eaf746a276e0e54bab184a635a7786b767aa56b4
Berk <berkfeyiz@outlook.com>
2018-11-22 15:55:19 +0200

    fourth commit

--- 20987_berkfeyiz_compress/20987_berkfeyiz_compress/20987_berkfeyiz_compress.cpp
+++ 20987_berkfeyiz_compress/20987_berkfeyiz_compress/20987_berkfeyiz_compress.cpp
@@ -18,7 +18,7 @@ void compress(){
 	char in;			// the name of the file that will be created
 	out_name = "compout.txt";		
 	ofstream compressed;	// the stream file of the created file
-	//compressed.open(out_name);
+	compressed.open(out_name);
 	ifstream readfile;
 	readfile.open("compin.txt");
     string input = "";
@@ -36,8 +36,9 @@ void compress(){
     char c;
     bool end = false;
     
+    
     int i = 256;
-    while(input != "\r\n\n" )
+    while(input != "\r\n\n")
     {
         int ccount = 1;
         p = input[0];
@@ -51,88 +52,40 @@ void compress(){
             c = input[ccount];
             pc = p + c;
             if(c== '\r')
-            {pc = p; end = true;}
+            {pc = p; end = true; break;}
         }
-        dictionary->add(pc, i);
+        if(i != 4096)
+        {dictionary->add(pc, i);}
+        
         if(p.length() == 1 && end == false)
         {
             cout << int(p.at(0)) << " ";
+            compressed << int(p.at(0)) << " ";
         }
         else if(p.length() > 1 && end == false)
         {
             cout << dictionary->fetch(p)->key << " ";
+            compressed << dictionary->fetch(p)->key << " ";
+        }
+        else if (end == true)
+        {
+            if (dictionary->contains(pc) == true)
+            {
+                cout <<dictionary->fetch(pc)->key << " " << endl;
+                compressed << dictionary->fetch(pc)->key << " " << endl;
+            }
+            
         }
         i++;
         input.erase(0, ccount);
         
     }
-    
-    
-   /* p = input[0];
-    c = input[1];
-    pc = p + c;
-    
-    cout << "p: "<< p << " c: " << c << " pc :" << pc << endl;
-    
-    p += c;
-    c = input[2];
-    pc = p + c;
-    
-    cout << "p: "<< p << " c: " << c << " pc :" << pc << endl;
-    
-    p += c;
-    c = input[3];
-    pc = p + c;
-    
-    cout << "p: "<< p << " c: " << c << " pc :" << pc << endl;
-
-    p += c;
-    c = input[4];
-    pc = p + c;
-    
-    cout << "p: "<< p << " c: " << c << " pc :" << pc << endl;
-    */
+    compressed.close();
 }
 
 int main() {
    
 	compress();
 	
-	
-	/* BST<int> *bst = new BST<int>();
-    bst->add(11);  bst->add(1);  bst->add(6); bst->add(-1); bst->add(-10); bst->add(100);
-    bst->print();
-    cout<<endl;
-    cout<<"Nodes count: "<<bst->nodesCount();
-    cout<<endl;
-    cout<<"Height: "<<bst->height();
-    cout<<endl;
-    cout<<"Max path: ";
-    bst->printMaxPath();
-    cout<<endl;
-    bst->deleteValue(11);
-    cout<<"11 removed: ";
-    bst->print();
-    cout<<endl;
-    cout<<"1 removed: ";
-    bst->deleteValue(1);
-    bst->print();
-    cout<<endl;
-    cout<<"-1 removed: ";
-    bst->deleteValue(-1);
-    bst->print();
-    cout<<endl;
-    cout<<"6 removed: ";
-    bst->deleteValue(6);
-    bst->print();
-    cout<<endl;
-    cout<<"-10 removed: ";
-    bst->deleteValue(-10);
-    bst->print();
-    cout<<endl;
-    cout<<"100 removed: ";
-    bst->deleteValue(100);
-    bst->print();
-    cout<<endl;*/
     return 0;
 }
